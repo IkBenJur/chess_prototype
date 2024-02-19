fcad45c3b2a373543b040786960cd4ef051e59e0
IkBenJur <jurspammer@gmail.com>
2024-02-19 12:44:42 +0100

    Changed tiles const public instead of private

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -1,22 +1,22 @@
 pub type Bitboard  = u64;
 
-const RANK1: Bitboard = 0b1111111100000000000000000000000000000000000000000000000000000000;
-const RANK2: Bitboard = RANK1 >> 8;
-const RANK3: Bitboard = RANK1 >> (8 * 2);
-const RANK4: Bitboard = RANK1 >> (8 * 3);
-const RANK5: Bitboard = RANK1 >> (8 * 4);
-const RANK6: Bitboard = RANK1 >> (8 * 5);
-const RANK7: Bitboard = RANK1 >> (8 * 6);
-const RANK8: Bitboard = RANK1 >> (8 * 7);
+pub const RANK1: Bitboard = 0b1111111100000000000000000000000000000000000000000000000000000000;
+pub const RANK2: Bitboard = RANK1 >> 8;
+pub const RANK3: Bitboard = RANK1 >> (8 * 2);
+pub const RANK4: Bitboard = RANK1 >> (8 * 3);
+pub const RANK5: Bitboard = RANK1 >> (8 * 4);
+pub const RANK6: Bitboard = RANK1 >> (8 * 5);
+pub const RANK7: Bitboard = RANK1 >> (8 * 6);
+pub const RANK8: Bitboard = RANK1 >> (8 * 7);
 
-const FILE_A: Bitboard = 0b1000000010000000100000001000000010000000100000001000000010000000;
-const FILE_B: Bitboard = FILE_A >> 1;
-const FILE_C: Bitboard = FILE_A >> 2;
-const FILE_D: Bitboard = FILE_A >> 3;
-const FILE_E: Bitboard = FILE_A >> 4;
-const FILE_F: Bitboard = FILE_A >> 5;
-const FILE_G: Bitboard = FILE_A >> 6;
-const FILE_H: Bitboard = FILE_A >> 7;
+pub const FILE_A: Bitboard = 0b1000000010000000100000001000000010000000100000001000000010000000;
+pub const FILE_B: Bitboard = FILE_A >> 1;
+pub const FILE_C: Bitboard = FILE_A >> 2;
+pub const FILE_D: Bitboard = FILE_A >> 3;
+pub const FILE_E: Bitboard = FILE_A >> 4;
+pub const FILE_F: Bitboard = FILE_A >> 5;
+pub const FILE_G: Bitboard = FILE_A >> 6;
+pub const FILE_H: Bitboard = FILE_A >> 7;
 
 pub trait BitManipulation {
     fn toggle_square(&self, index: u8) -> Bitboard;
