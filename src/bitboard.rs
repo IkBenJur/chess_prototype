c796675d00bb062cd6137956da18adcaa22d8294
IkBenJur <jurspammer@gmail.com>
2024-02-21 12:31:17 +0100

    Fixed bug. All file bitboards where map the oppisite way.

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -9,14 +9,14 @@ pub const RANK6: Bitboard = RANK1 >> (8 * 5);
 pub const RANK7: Bitboard = RANK1 >> (8 * 6);
 pub const RANK8: Bitboard = RANK1 >> (8 * 7);
 
-pub const FILE_A: Bitboard = 0b1000000010000000100000001000000010000000100000001000000010000000;
-pub const FILE_B: Bitboard = FILE_A >> 1;
-pub const FILE_C: Bitboard = FILE_A >> 2;
-pub const FILE_D: Bitboard = FILE_A >> 3;
-pub const FILE_E: Bitboard = FILE_A >> 4;
-pub const FILE_F: Bitboard = FILE_A >> 5;
-pub const FILE_G: Bitboard = FILE_A >> 6;
-pub const FILE_H: Bitboard = FILE_A >> 7;
+pub const FILE_A: Bitboard = 0b0000000100000001000000010000000100000001000000010000000100000001;
+pub const FILE_B: Bitboard = FILE_A << 1;
+pub const FILE_C: Bitboard = FILE_A << 2;
+pub const FILE_D: Bitboard = FILE_A << 3;
+pub const FILE_E: Bitboard = FILE_A << 4;
+pub const FILE_F: Bitboard = FILE_A << 5;
+pub const FILE_G: Bitboard = FILE_A << 6;
+pub const FILE_H: Bitboard = FILE_A << 7;
 
 pub trait BitManipulation {
     fn toggle_square(&self, index: u8) -> Bitboard;
